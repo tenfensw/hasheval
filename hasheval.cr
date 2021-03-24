ad6ee57757408f091ff0ceefc479340651144b3f
Tim K <timprogrammer@rambler.ru>
2021-03-24 07:40:18 +0300

    something that looks like a copyright header added to the module entrypoint

--- hasheval.cr
+++ hasheval.cr
@@ -1,1 +1,11 @@
+#
+# HashEval - evaluate stringified hashes to Crystal
+# ones
+#
+# Copyright (C) Tim K <timprogrammer@rambler.ru> 2021
+#
+# License: MIT
+# Repository: https://github.com/tenfensw/hasheval
+#
+
 require "./hasheval/eval.cr"
